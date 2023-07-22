211785b23bee568cdf7c2cabd708427c968e9cab
sachin <sachin14051@gmail.com>
2023-07-23 00:54:59 +0530

    working on event bubbling and event capturing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 let cl = console.log;
 const parent = document.getElementById("parent");
 const child = document.getElementById("child")
+const info = document.getElementById("info")
+
 
 
 
@@ -25,3 +27,11 @@ child.addEventListener("click",childCall,true);
 //event capturing
 parent.addEventListener("click",parentCall,false);
 child.addEventListener("click",childCall,false)
+
+info.innerHTML = ` event bubbling
+parent.addEventListener("click",parentCall,true);
+child.addEventListener("click",childCall,true);
+<br>
+event capturing
+parent.addEventListener("click",parentCall,false);
+child.addEventListener("click",childCall,false)`
